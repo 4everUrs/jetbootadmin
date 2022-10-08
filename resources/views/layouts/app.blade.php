3d3c19e0913398c7aeaef4d0521dd1d55bfc2b60
Techie Trendz <techietrendz@email.com>
2022-10-08 23:42:27 +0800

    hahaha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,7 +128,7 @@
     </div>
     <!-- ./wrapper -->
     
-    {{-- <script>
+    <script>
         window.addEventListener('showButton', event => {
         var element = document.getElementById("createButton");
         var review = document.getElementById("reviewButton");
@@ -174,14 +174,14 @@
             body.classList.remove("dark-mode");
             topbar.classList.remove('bg-dark');
         }
-    </script> --}}
+    </script>
     @stack('modals')
     
     @livewireScripts
     
     @stack('scripts')
     
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
     
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script src="{{asset('dist/js/adminlte.js')}}"></script>
@@ -214,7 +214,7 @@
 
     <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 
-    <script src="{{asset('dist/js/pages/dashboard.js')}}"></script>  --}}
+    <script src="{{asset('dist/js/pages/dashboard.js')}}"></script> 
 </body>
 
 </html>
