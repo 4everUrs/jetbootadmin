95277f825b191ddad2a7170b75f953ba316a3390
Techie Trendz <techietrendz@email.com>
2022-10-08 21:09:06 +0800

    merge sidebar

--- resources/views/livewire/admin/users-list.blade.php
+++ resources/views/livewire/admin/users-list.blade.php
@@ -72,6 +72,7 @@
                 <div class="col">
                     <label>Department</label>
                     <select wire:model="dept" class="form-control">
+                        <option value="">Select Option</option>
                           @if (!empty($teams)){
                             @foreach ($teams as $key => $team)
                                 @if ($team->name != 'Admin')
@@ -87,6 +88,7 @@
                <div class="col d-none" id="department">
                     <label>XYZ</label>
                     <select wire:model="department_id" class="form-control">
+                        <option value="">Select Option</option>
                         @if (!empty($departments)){
                             @foreach ($departments as $key => $department)
                                 <option >{{$department->name}}</option>
