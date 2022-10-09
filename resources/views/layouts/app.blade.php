5437116237acc1d4d690fa2c7046ed49d8ad14fb
Techie Trendz <techietrendz@email.com>
2022-10-09 12:55:06 +0800

    hahaha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -180,10 +180,10 @@
         }
     </script>
     
-    
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
     
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    
     <script src="{{asset('dist/js/adminlte.js')}}"></script>
     
     <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
