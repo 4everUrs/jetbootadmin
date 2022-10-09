ef5701ad28a8936b569ee3a371c6342934423901
Techie Trendz <techietrendz@email.com>
2022-10-09 12:47:44 +0800

    hahaha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -29,8 +29,7 @@
     <!-- summernote -->
     <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
     @livewireStyles
-    
-    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed" id="boody">
@@ -128,13 +127,59 @@
         <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
-    
-    
     @stack('modals')
     
     @livewireScripts
     
     @stack('scripts')
+    <script>
+        window.addEventListener('showButton', event => {
+        var element = document.getElementById("createButton");
+        var review = document.getElementById("reviewButton");
+        element.classList.remove("d-none");
+        review.classList.add("d-none");
+        })
+        window.addEventListener('vendor-form', event => {
+        var vendor = document.getElementById('vendor');
+        var content = document.getElementById('content');
+        content.classList.add('d-none');
+        vendor.classList.remove('d-none');
+        })
+       /** add active class and stay opened when selected */
+        var url = window.location;
+        const allLinks = document.querySelectorAll('.nav-item a');
+        const currentLink = [...allLinks].filter(e => {
+        return e.href == url;
+        });
+        
+        if (currentLink.length > 0) { //this filter because some links are not from menu
+        currentLink[0].classList.add("active");
+        currentLink[0].closest(".nav-treeview").style.display = "block";
+        currentLink[0].closest(".nav-treeview").classList.add("active");
+        }
+        
+        function darkmode() {
+            var body = document.getElementById("boody");
+            var topbar = document.getElementById('topbar');
+            var moon = document.getElementById('dark');
+            var sun = document.getElementById('light');
+            sun.classList.remove('d-none');
+            moon.classList.add('d-none');
+            body.classList.add("dark-mode");
+            topbar.classList.add('bg-dark');
+        }
+        function light() {
+            var body = document.getElementById("boody");
+            var topbar = document.getElementById('topbar');
+            var moon = document.getElementById('dark');
+            var sun = document.getElementById('light');
+            sun.classList.add('d-none');
+            moon.classList.remove('d-none');
+            body.classList.remove("dark-mode");
+            topbar.classList.remove('bg-dark');
+        }
+    </script>
+    
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
     
@@ -168,7 +213,7 @@
     <script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
 
     <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-    <script src="{{ mix('js/app.js') }}" defer></script>
+
     {{-- <script src="{{asset('dist/js/pages/dashboard.js')}}"></script>  --}}
 </body>
 
