ba70e31145db8c3f75032f72ed80bb27fa548c45
Techie Trendz <techietrendz@email.com>
2022-10-11 12:15:24 +0800

    core

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,36 +4,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
+    <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
-    <!-- Ionicons -->
+    <!-- IonIcons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="{{asset('plugins/jqvmap/jqvmap.min.css')}}">
     <!-- Theme style -->
-    
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker.css')}}">
-    <!-- summernote -->
-    <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
-    @livewireStyles
-    @yield('style')
-    
-</head>
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed" id="boody">
+    @livewireStyles
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<!--
+`body` tag options:
+
+  Apply one or more of the following classes to to the body tag
+  to get the desired effect
+
+  * sidebar-collapse
+  * sidebar-mini
+-->
+
+<body class="hold-transition sidebar-mini">
     <div class="wrapper">
         <!-- Navbar -->
         @include('layouts.topbar')
@@ -43,8 +39,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Tech-Trendz</span>
             </a>
 
@@ -53,11 +49,12 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}"
+                            class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="{{route('profile.show')}}" class="d-block">{{ Auth::user()->name }}</a>
-                        
+
                     </div>
                 </div>
                 @if (Auth::user()->current_team_id == '1')
@@ -76,7 +73,6 @@
             </div>
             <!-- /.sidebar -->
         </aside>
-
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
@@ -86,122 +82,50 @@
                         <div class="col-sm-6">
                             <h1 class="m-0">{{ $header }}</h1>
                         </div><!-- /.col -->
-                       
+
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
             </div>
-            
+
             <div class="content">
                 <div class="container-fluid">
                     {{$slot}}
                 </div>
             </div>
-           
-            <!-- /.content -->
+
         </div>
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <strong>Disclamer: This is for educational purposes only. Provided by the student of <a href="https://bcp.edu.ph/">Bestlink College of the Philippines</a></strong>
-           
-        </footer>
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
-    </div>
-    <!-- ./wrapper -->
-    
-    <script>
-        window.addEventListener('showButton', event => {
-        var element = document.getElementById("createButton");
-        var review = document.getElementById("reviewButton");
-        element.classList.remove("d-none");
-        review.classList.add("d-none");
-        })
-        window.addEventListener('vendor-form', event => {
-        var vendor = document.getElementById('vendor');
-        var content = document.getElementById('content');
-        content.classList.add('d-none');
-        vendor.classList.remove('d-none');
-        })
-       /** add active class and stay opened when selected */
-        var url = window.location;
-        const allLinks = document.querySelectorAll('.nav-item a');
-        const currentLink = [...allLinks].filter(e => {
-        return e.href == url;
-        });
-        
-        if (currentLink.length > 0) { //this filter because some links are not from menu
-        currentLink[0].classList.add("active");
-        currentLink[0].closest(".nav-treeview").style.display = "block";
-        currentLink[0].closest(".has-treeview").classList.add("active");
-        }
-        
-        function darkmode() {
-            var body = document.getElementById("boody");
-            var topbar = document.getElementById('topbar');
-            var moon = document.getElementById('dark');
-            var sun = document.getElementById('light');
-            sun.classList.remove('d-none');
-            moon.classList.add('d-none');
-            body.classList.add("dark-mode");
-            topbar.classList.add('bg-dark');
-        }
-        function light() {
-            var body = document.getElementById("boody");
-            var topbar = document.getElementById('topbar');
-            var moon = document.getElementById('dark');
-            var sun = document.getElementById('light');
-            sun.classList.add('d-none');
-            moon.classList.remove('d-none');
-            body.classList.remove("dark-mode");
-            topbar.classList.remove('bg-dark');
-        }
-    </script>
-    
-    <script src="{{ mix('js/app.js') }}" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-    
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-    <script src="{{asset('dist/js/adminlte.js')}}"></script>
-    
-    <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 
-    <script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-    @stack('modals')
-    
-    @livewireScripts
-    
-    @stack('scripts')
-    <script>
-        $.widget.bridge('uibutton', $.ui.button)
-    </script>
 
-    <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
-    <script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
 
-    {{-- <script src="{{asset('plugins/sparklines/sparkline.js')}}"></script> --}}
+        <!-- jQuery -->
+        <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 
-    <script src="{{asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-    <script src="{{asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+        <!-- Bootstrap -->
+        <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <!-- AdminLTE -->
+        <script src="{{asset('dist/js/adminlte.js')}}"></script>
 
-    <script src="{{asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 
-    <script src="{{asset('plugins/moment/moment.min.js')}}"></script>
-    <script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+        <script>
+            var url = window.location;
+            
+            // for sidebar menu entirely but not cover treeview
+            $('ul.nav-sidebar a').filter(function() {
+            return this.href == url;
+            }).addClass('active');
+            
+            // for treeview
+            $('ul.nav-treeview a').filter(function() {
+            return this.href == url;
+            }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+        </script>
+        @stack('modals')
 
-    <script src="{{asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+        @livewireScripts
 
-    <script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
-
-    <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-
-    
-
-    <script src="{{asset('dist/js/pages/dashboard.js')}}"></script> 
+        @stack('scripts')
 </body>
 
 </html>
