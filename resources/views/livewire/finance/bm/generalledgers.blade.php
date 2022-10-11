c11a1e3defe5c46b2b3ad25ba0f248520d60d4a5
Finance <spampanga0614@gmail.com>
2022-10-11 21:05:51 +0800

    hfdfg

--- resources/views/livewire/finance/bm/generalledgers.blade.php
+++ resources/views/livewire/finance/bm/generalledgers.blade.php
@@ -9,51 +9,6 @@
 <a class="btn btn-info"href="{{route('generalreports')}}">Download PDF</a>
 <div class="card">
     <div class="card-body">
-
-        <x-table head="General Report ">
-            <thead>
-                <th></th>
-                <th>2022</th>
-                
-            </thead>
-        <tbody>
-            <tr>
-                <td><h2>Liabilities</h2></td>
-                <td></td>
-                
-            </tr>
-            <tr>
-                <td>Accounts Payable</td>
-                <td></td>
-                
-            </tr>
-            <tr>
-                <td>Income Tax Payable</td>
-                <td></td>
-                
-            </tr>
-            <tr>
-                <td>Interest Payable</td>
-                <td></td>
-                
-            </tr>
-            <tr>
-                <td>Accrued Expenses</td>
-                <td></td>
-                
-            </tr>
-            <tr>
-                <td>Unearned Payable</td>
-                <td></td>
-                
-            </tr>
-                
-                
-             
-        </tbody>
-
-
-         </x-table>
     </div>
 </div>
 </div>
