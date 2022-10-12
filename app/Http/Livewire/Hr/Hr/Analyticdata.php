518be8c1685784170cb95b1c0c089e1fad55c894
Cyril Destreza <sayril.cyril@gmail.com>
2022-10-12 13:05:32 +0800

    gagagha

--- app/Http/Livewire/Hr/Hr/Analyticdata.php
+++ app/Http/Livewire/Hr/Hr/Analyticdata.php
@@ -7,8 +7,19 @@ use App\Models\Analytic;
 
 class Analyticdata extends Component
 {
+    public $label, $price;
+    
     public function render()
     {
         return view('livewire.hr.hr.analyticdata');
+    }
+    public function analytics(){
+
+            $label = ['Shirts', 'T-Shirt', 'Pants', 'Coat', 'Kurta', 'Pajama'];
+            $price = ['10', '5', '100', '90', '50', '30'];
+            return view('livewire.hr.hr.analyticdata',['labels' => $label, 'prices' => $price]);
+        
+
+
     }
 }
