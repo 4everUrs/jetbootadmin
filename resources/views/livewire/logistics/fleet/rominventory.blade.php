915b808d53fd5f17b849f101f0e828ee4e47c110
Techie Trendz <techietrendz@email.com>
2022-10-17 01:02:51 +0800

    fixed

--- resources/views/livewire/logistics/fleet/rominventory.blade.php
+++ resources/views/livewire/logistics/fleet/rominventory.blade.php
@@ -6,17 +6,23 @@
     </x-slot>
     <div class="card">
         <div class="card-body">
+            <button class="btn btn-dark btn-sm">Request Item</button>
             <x-table head="Inventory List">
-                <thead>
-                    <th>PLACE HOLDER</th>
-                    <th>PLACE HOLDER</th>
-                    <th>PLACE HOLDER</th>
-                    <th>PLACE HOLDER</th>
-                    <th>PLACE HOLDER</th>
+                <thead class="bg-info">
+                    <th>Item ID</th>
+                    <th>Item Name</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Unit Price</th>
+                    <th>Inventory Value</th>
+                    <th>Action</th>
                 </thead>
-                <tr>
-                        <td colspan="6" class="text-center">No Record Found</td>
+                <tbody>
+                    <tr>
+                        <td class="text-center" colspan="7">No Record Found</td>
                     </tr>
+                </tbody>
             </x-table>
-
+        </div>
+    </div>
 </div>
