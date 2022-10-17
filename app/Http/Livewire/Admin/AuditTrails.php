d437f94bcdddc01cd193079bb331e6711582dea7
Techie Trendz <techietrendz@email.com>
2022-10-17 20:09:22 +0800

    save

--- app/Http/Livewire/Admin/AuditTrails.php
+++ app/Http/Livewire/Admin/AuditTrails.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Audit as ModelsAudit;
+use App\Models\Stock;
 use Livewire\Component;
+use Livewire\WithPagination;
 use OwenIt\Auditing\Models\Audit;
 
 class AuditTrails extends Component
 {
-
+    use WithPagination;
+    public $audits;
     public function render()
     {
         return view('livewire.admin.audit-trails');
+    }
+    public function mount()
+    {
+        $this->audits = Stock::first()->audits()->latest()->first()->getMetaData();
     }
 }
