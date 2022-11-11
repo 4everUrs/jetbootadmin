2e6f6b7c35e1e06a8e0d2ef8443c57347921f8f5
Finance <spampanga0614@gmail.com>
2022-11-11 23:18:07 +0800

    hhhhh

--- resources/views/livewire/finance/sidebar.blade.php
+++ resources/views/livewire/finance/sidebar.blade.php
@@ -16,46 +16,46 @@
                 </a>
                 <ul class="nav nav-treeview"> 
                     <li class="nav-item">
-                        <a href="{{route('transaction')}}" class="nav-link"></i>Transaction</a>
+                        <a href="{{route('transaction')}}" class="nav-link"><i class="fas fa-share"></i>&nbsp;Transaction</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('requestlist')}}" class="nav-link">Request lists</a>
+                        <a href="{{route('requestlist')}}" class="nav-link"><i class="fas fa-bell"></i>&nbsp;Request lists</a>
                         
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('allocatebudget')}}" class="nav-link">Budget Allocation</a>
+                        <a href="{{route('allocatebudget')}}" class="nav-link"><i class="fas fa-piggy-bank"></i>&nbsp;Budget Allocation</a>
                     </li>
                 </ul>
             </li> 
             <li class="nav-item">
                 <a href="#disbursement" class="nav-link">
                     {{--DISBURSEMENT --}}
-                    <i class="fas fa-calculator"></i>
+                    <i class="fas fa-money-bill"></i>
                     <p>Disbursement</p>
                     <i class="right fas fa-angle-left"></i>
                 </a>
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
-                        <a href="{{route('moneyTransaction')}}" class="nav-link"></i>Budget Transaction</a>
+                        <a href="{{route('moneyTransaction')}}"class="nav-link"><i class="fas fa-chart-pie"></i>&nbsp;Budget Transaction</a>
                     </li>
                 </ul>
             </li>
             <li class="nav-item">
                 <a href="#ap&ar" class="nav-link">
                     {{--AP & AR  --}}
-                    <i class="fas fa-calculator"></i>
+                    <i class="fas fa-folder-open"></i>
                     <p>AP & AR</p>
                     <i class="right fas fa-angle-left"></i>
                 </a>
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
-                        <a href="{{route('journal')}}" class="nav-link">Records and Reports</a>
+                        <a href="{{route('journal')}}" class="nav-link"><i class="fas fa-file-import"></i>&nbsp;Records and Reports</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('listspayables')}}" class="nav-link">Lists of Payables</a>
+                        <a href="{{route('listspayables')}}" class="nav-link"><i class="fas fa-receipt"></i>&nbsp;Lists of Payables</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{route('listsreceivable')}}" class="nav-link">Lists of Receivable</a>
+                        <a href="{{route('listsreceivable')}}" class="nav-link"><i class="fas fa-arrow-down"></i>Lists of Receivable</a>
                     </li>
                     
                 </ul>
@@ -63,13 +63,14 @@
 
             <li class="nav-item">
                 <a href="#collection" class="nav-link">
-                <i class="fas fa-calculator"></i>
+                    <i class="fas fa-cash-register"></i>
                     <p>Collection</p>
                     <i class="right fas fa-angle-left"></i>
                 </a>
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
-                        <a href="{{route('collects')}}" class="nav-link">Earnings</a>
+                        
+                        <a href="{{route('collects')}}" class="nav-link"><i class="fas fa-coins"></i>Earnings</a>
                     </li>
                     
                 </ul>
